b1e4afa8f61a63a4a4f225cb5e9b160a06b20f55
giftthedeveloper <giftjeremiah001@gmail.com>
2023-09-27 13:24:13 +0100

    feat: worked on encoding and code Tables

--- src/main.ts
+++ src/main.ts
@@ -51,6 +51,59 @@ function buildTree(frequencyMap: Map<string, number>): TreeNode {
 }
 
 
+interface CodeTable {
+    [character: string]: string;
+  }
+  
+  //function to generate the code table based on the tree. 
+  //the helps to store the code for a sepcific character based on the huffman tree for encoding & decoding.
+  function generateCodeTable(root: TreeNode): CodeTable {
+    const codeTable: CodeTable = {};
+  
+    function traverse(node: TreeNode, currentCode: string) {
+      if (node.character) {
+        codeTable[node.character] = currentCode;
+      }
+      if (node.left) {
+        traverse(node.left, currentCode + '0'); 
+      }
+      if (node.right) {
+        traverse(node.right, currentCode + '1'); 
+      }
+    }
+  
+    traverse(root, ''); 
+  
+    return codeTable;
+  }
+  
+  //stores and jsonify character mappings information (for easy decoding and encoding) for decoding algorithm 
+  function WriteHeaderSection(outputFileName: string, codeTable: CodeTable): boolean {
+    try {
+      const headerData = JSON.stringify(codeTable);
+      fs.writeFileSync(outputFileName, headerData);
+      return true;
+
+    } catch (error) {
+      console.error('Error writing header:', error);
+          return false;
+    }
+  }
+  
+
+  function encodeText(text: string, codeTable: CodeTable): string {
+    let encodedData = '';
+    for (const char of text) {
+      encodedData += codeTable[char];
+    }
+    return encodedData;
+  }
+  
+  function writeCompressedText(outputFileName: string, encodedData: string) {
+    fs.appendFileSync(outputFileName, encodedData);
+  }
+  
+
 //if the filename is not provided on the command line throw error
 if (process.argv.length < 3) {
   console.error('Please provide a file path as a command-line argument.');
@@ -59,11 +112,25 @@ if (process.argv.length < 3) {
 
 //get the filename from the cli
 const filename = process.argv[2]; 
+console.log(filename)
 const frequencyMap = countCharFrequency(filename);
 const root = buildTree(frequencyMap);
-console.log(root);
+const codeTable = generateCodeTable(root);
+const outputFileName = `compressed-${filename}`
+console.log(outputFileName)
+const headerSection = WriteHeaderSection(outputFileName, codeTable);
 
+if (!headerSection) {
+  console.error('Failed to write the header.');
+} else {
+  const encodedData = encodeText(filename, codeTable);
+  console.log(encodedData, headerSection)
+  writeCompressedText(outputFileName, encodedData);
 
-for (const [char, frequency] of frequencyMap.entries()) {
-  console.log(`Character: ${char}, Frequency: ${frequency}`);
+  console.log('Compression completed successfully.');
 }
+
+
+// for (const [char, frequency] of frequencyMap.entries()) {
+//   console.log(`Character: ${char}, Frequency: ${frequency}`);
+// }
